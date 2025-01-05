386bf1ae859fa738e2ed7b200a3bd4456cf6a29e
Mitchelada <michel.araya@live.com>
2025-01-05 18:57:38 -0300

    feat: cypress test e2e

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    supportFile: false, // Desactiva el uso del archivo de soporte
+    setupNodeEvents() {
+      // implementa eventos si es necesario
+    },
+    baseUrl: 'http://localhost:5000', // Ajusta esto según tu proyecto
+  },
+})
